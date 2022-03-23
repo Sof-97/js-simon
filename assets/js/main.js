4b60a770eba0dfcc951c7a37fe72113c24212614
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-23 18:42:05 +0100

    Funzioni numeri

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
-let numToGuess = [];
+let numToGuess;
+let start = document.getElementById('start');
+let toGuess = document.getElementById('toGuess');
 
+
+
+start.addEventListener('click',
+    function () {
+        numToGuessGen(),
+        showNumbers(),
+        removeNumbers()
+    }
+)
 
 
 //NOTE Generazione numeri da indovinare
@@ -8,7 +19,24 @@ function numGen(z) {
     return x;
 }
 
-for (i = 0; i < numToGuess.length; i++) {
-    let num = numGen(100);
-    
+function numToGuessGen() {
+    numToGuess = [];
+    //NOTE Riempimento array numeri
+    while (numToGuess.length < 5) {
+        let num = numGen(100);
+        if (!numToGuess.includes(num)) {
+            numToGuess.push(num);
+        }
+    }
+}
+
+//NOTE Stampavalori a schermo
+function showNumbers(){
+    toGuess = '';
+    for(i=0; i<numToGuess.length; i++){
+        let box = document.createElement('div');
+        box.classList.add('numbers');
+        box.innerHTML= (`${(numToGuess[i])}`);
+        document.getElementById('toGuess').appendChild(box);
+    }
 }
